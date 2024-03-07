d2224353a07a741d28767d6585120a9c6f5fed06
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-06 22:07:19 -0500

    adding modal structure to build form to fit into the styles using a container and add table

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ const data = [
 const App = () => {
   state = {
     data: data,
-   
+    modalActualizar: false,
+    modalInsertar: false,
     form: {
       id: "",
       origen: "",
@@ -30,7 +31,219 @@ const App = () => {
     },
   };
   return (
-   <h1>CRUD</h1>
+    <>
+    <Container>
+    <br />
+      <Button color="success" >Crear</Button>
+      <br />
+      <br />
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Origen</th>
+            <th>Destino</th>
+            <th>Fecha</th>
+            <th>Número de vuelo</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.state.data.map((dato) => (
+            <tr key={dato.id}>
+              <td>{dato.id}</td>
+              <td>{dato.origen}</td>
+              <td>{dato.destino}</td>
+              <td>{dato.date}</td>
+              <td>{dato.numvuelo}</td>
+              <td>
+                <Button
+                  color="primary"
+                  
+                >
+                  Editar
+                </Button>{" "}
+                <Button color="danger" >Eliminar</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+
+    <Modal isOpen={this.state.modalActualizar}>
+      <ModalHeader>
+       <div><h3>Editar Registro</h3></div>
+      </ModalHeader>
+
+      <ModalBody>
+        <FormGroup>
+          <label>
+           Id:
+          </label>
+        
+          <input
+            className="form-control"
+            readOnly
+            type="text"
+            value={this.state.form.id}
+          />
+        </FormGroup>
+        
+        <FormGroup>
+          <label>
+            Origen: 
+          </label>
+          <input
+            className="form-control"
+            name="origen"
+            type="text"
+
+          />
+        </FormGroup>
+        
+        <FormGroup>
+          <label>
+            Destino: 
+          </label>
+          <input
+            className="form-control"
+            name="destino"
+            type="text"
+      
+            
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <label>
+            Fecha: 
+          </label>
+          <input
+            className="form-control"
+            name="date"
+            type="date"
+      
+      
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <label>
+            Número de Vuelo: 
+          </label>
+          <input
+            className="form-control"
+            name="numvuelo"
+            type="text"
+    
+            
+          />
+        </FormGroup>
+
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          color="primary"
+          
+        >
+          Editar
+        </Button>
+        <Button
+          color="danger"
+        >
+          Cancelar
+        </Button>
+      </ModalFooter>
+    </Modal>
+
+
+
+    <Modal>
+      <ModalHeader>
+       <div><h3>Insertar Registro</h3></div>
+      </ModalHeader>
+
+      <ModalBody>
+        <FormGroup>
+          <label>
+            Id: 
+          </label>
+          
+          <input
+            className="form-control"
+            readOnly
+            type="text"
+          />
+        </FormGroup>
+        
+        <FormGroup>
+          <label>
+            Origen: 
+          </label>
+          <input
+            className="form-control"
+            name="Origen"
+            type="text"
+           
+          />
+        </FormGroup>
+        
+        <FormGroup>
+          <label>
+            Destino: 
+          </label>
+          <input
+            className="form-control"
+            name="destino"
+            type="text"
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <label>
+            Fecha: 
+          </label>
+          <input
+            className="form-control"
+            name="date"
+            type="date"
+        
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <label>
+            Número de Vuelo: 
+          </label>
+          <input
+            className="form-control"
+            name="numvuelo"
+            type="text"
+        
+          />
+        </FormGroup>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          color="primary"
+
+        >
+          Insertar
+        </Button>
+        <Button
+          className="btn btn-danger"
+          
+        >
+          Cancelar
+        </Button>
+      </ModalFooter>
+    </Modal>
+  </>
   )
 }
 
