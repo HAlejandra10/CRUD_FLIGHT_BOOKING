6df9b1fbf8baa3141a83811ee38c16d598cb13d1
Heidy Primo <heidyprimo10@hotmail.com>
2024-03-06 22:35:41 -0500

    adding events and functions to add to the buttons to make interaction and changes into the form

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const data = [
     {id:2, origen: 'Ciudad C', destino: 'Ciudad D', date: '2024-03-15', numvuelo: 'DEF456' },
 ];
 
-const App = () => {
+class App extends React.Component {
   state = {
     data: data,
     modalActualizar: false,
@@ -30,221 +30,295 @@ const App = () => {
       numvuelo: "",
     },
   };
-  return (
-    <>
-    <Container>
-    <br />
-      <Button color="success" >Crear</Button>
-      <br />
-      <br />
-      <Table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Origen</th>
-            <th>Destino</th>
-            <th>Fecha</th>
-            <th>Número de vuelo</th>
 
-          </tr>
-        </thead>
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
 
-        <tbody>
-          {this.state.data.map((dato) => (
-            <tr key={dato.id}>
-              <td>{dato.id}</td>
-              <td>{dato.origen}</td>
-              <td>{dato.destino}</td>
-              <td>{dato.date}</td>
-              <td>{dato.numvuelo}</td>
-              <td>
-                <Button
-                  color="primary"
-                  
-                >
-                  Editar
-                </Button>{" "}
-                <Button color="danger" >Eliminar</Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
 
-    <Modal isOpen={this.state.modalActualizar}>
-      <ModalHeader>
-       <div><h3>Editar Registro</h3></div>
-      </ModalHeader>
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
 
-      <ModalBody>
-        <FormGroup>
-          <label>
-           Id:
-          </label>
-        
-          <input
-            className="form-control"
-            readOnly
-            type="text"
-            value={this.state.form.id}
-          />
-        </FormGroup>
-        
-        <FormGroup>
-          <label>
-            Origen: 
-          </label>
-          <input
-            className="form-control"
-            name="origen"
-            type="text"
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
 
-          />
-        </FormGroup>
-        
-        <FormGroup>
-          <label>
-            Destino: 
-          </label>
-          <input
-            className="form-control"
-            name="destino"
-            type="text"
-      
-            
-          />
-        </FormGroup>
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id === registro.id) {
+        arreglo[contador].origen = dato.origen;
+        arreglo[contador].destino = dato.destino;
+        arreglo[contador].date = dato.date;
+        arreglo[contador].numvuelo = dato.numvuelo;
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
 
-        <FormGroup>
-          <label>
-            Fecha: 
-          </label>
-          <input
-            className="form-control"
-            name="date"
-            type="date"
-      
-      
-          />
-        </FormGroup>
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
+    if (opcion === true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id === registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
 
-        <FormGroup>
-          <label>
-            Número de Vuelo: 
-          </label>
-          <input
-            className="form-control"
-            name="numvuelo"
-            type="text"
+  insertar= ()=>{
+    var valorNuevo= {...this.state.form};
+    valorNuevo.id=this.state.data.length+1;
+    var lista= this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
     
+    return (
+      <>
+        <Container>
+        <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <br />
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Origen</th>
+                <th>Destino</th>
+                <th>Fecha</th>
+                <th>Número de vuelo</th>
+
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.origen}</td>
+                  <td>{dato.destino}</td>
+                  <td>{dato.date}</td>
+                  <td>{dato.numvuelo}</td>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
             
-          />
-        </FormGroup>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Origen: 
+              </label>
+              <input
+                className="form-control"
+                name="origen"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.origen}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Destino: 
+              </label>
+              <input
+                className="form-control"
+                name="destino"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.destino}
+              />
+            </FormGroup>
 
-      </ModalBody>
+            <FormGroup>
+              <label>
+                Fecha: 
+              </label>
+              <input
+                className="form-control"
+                name="date"
+                type="date"
+                onChange={this.handleChange}
+                value={this.state.form.date}
+              />
+            </FormGroup>
 
-      <ModalFooter>
-        <Button
-          color="primary"
-          
-        >
-          Editar
-        </Button>
-        <Button
-          color="danger"
-        >
-          Cancelar
-        </Button>
-      </ModalFooter>
-    </Modal>
+            <FormGroup>
+              <label>
+                Número de Vuelo: 
+              </label>
+              <input
+                className="form-control"
+                name="numvuelo"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.numvuelo}
+              />
+            </FormGroup>
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
 
 
 
-    <Modal>
-      <ModalHeader>
-       <div><h3>Insertar Registro</h3></div>
-      </ModalHeader>
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Registro</h3></div>
+          </ModalHeader>
 
-      <ModalBody>
-        <FormGroup>
-          <label>
-            Id: 
-          </label>
-          
-          <input
-            className="form-control"
-            readOnly
-            type="text"
-          />
-        </FormGroup>
-        
-        <FormGroup>
-          <label>
-            Origen: 
-          </label>
-          <input
-            className="form-control"
-            name="Origen"
-            type="text"
-           
-          />
-        </FormGroup>
-        
-        <FormGroup>
-          <label>
-            Destino: 
-          </label>
-          <input
-            className="form-control"
-            name="destino"
-            type="text"
-            onChange={this.handleChange}
-          />
-        </FormGroup>
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id: 
+              </label>
+              
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length+1}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Origen: 
+              </label>
+              <input
+                className="form-control"
+                name="Origen"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Destino: 
+              </label>
+              <input
+                className="form-control"
+                name="destino"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
 
-        <FormGroup>
-          <label>
-            Fecha: 
-          </label>
-          <input
-            className="form-control"
-            name="date"
-            type="date"
-        
-          />
-        </FormGroup>
+            <FormGroup>
+              <label>
+                Fecha: 
+              </label>
+              <input
+                className="form-control"
+                name="date"
+                type="date"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
 
-        <FormGroup>
-          <label>
-            Número de Vuelo: 
-          </label>
-          <input
-            className="form-control"
-            name="numvuelo"
-            type="text"
-        
-          />
-        </FormGroup>
-      </ModalBody>
+            <FormGroup>
+              <label>
+                Número de Vuelo: 
+              </label>
+              <input
+                className="form-control"
+                name="numvuelo"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
 
-      <ModalFooter>
-        <Button
-          color="primary"
-
-        >
-          Insertar
-        </Button>
-        <Button
-          className="btn btn-danger"
-          
-        >
-          Cancelar
-        </Button>
-      </ModalFooter>
-    </Modal>
-  </>
-  )
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
-
-export default App
+export default App;
